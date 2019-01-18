a3edf3741d44ee94cdbd3b48479f395695eef1a1
Elizabeth Nakooli <enakooli92@gmail.com>
2019-01-18 09:12:26 +0300

    experience table

---
+++ database/migrations/2019_01_18_055621_create_experiences_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateExperiencesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('experiences', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade'); 
+            $table->string('title')->nullable()->change();
+            $table->string('company')->nullable()->change();
+            $table->year('from_period')->nullable()->change();
+            $table->year('to_period')->nullable()->change();
+            $table->char('is_current', 1)->nullable()-change();
+            $table->text('description')->nullable()->change();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('experiences');
+    }
+}
