dcb8f6512069d4be1816d34d1e8109de22e9d760
Elizabeth Nakooli <enakooli92@gmail.com>
2019-01-18 09:42:45 +0300

    profile skills controller

---
+++ app/Http/Controllers/ProfileSkillsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Profile_Skills;
+use Illuminate\Http\Request;
+
+class ProfileSkillsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Profile_Skills  $profile_Skills
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Profile_Skills $profile_Skills)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Profile_Skills  $profile_Skills
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Profile_Skills $profile_Skills)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Profile_Skills  $profile_Skills
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Profile_Skills $profile_Skills)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Profile_Skills  $profile_Skills
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Profile_Skills $profile_Skills)
+    {
+        //
+    }
+}
