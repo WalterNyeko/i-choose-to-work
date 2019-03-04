ad2b3ffa536f4c34251b86193491b4798224d30e
maron <maronwhite6@gmail.com>
2019-03-04 17:18:24 +0300

    Partner Module
    Index and Show api end points

---
+++ app/Http/Controllers/Api/PartnerController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Resources\PartnerCollection;
+use App\Http\Resources\Partner as PartnerResource;
+use App\Models\Partner;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PartnerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $partners = Partner::all();
+        return new PartnerCollection($partners);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //Handle exception incase the model is not found
+        try {
+            $partner = Partner::findOrFail($id);
+        } catch (ModelNotFoundException $e) {
+            return response()->json(['message', 'The partner is either deleted or not existing']);
+        }
+        return new PartnerResource($partner);
+    }
+
+}
