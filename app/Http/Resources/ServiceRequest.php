9f902f0ae5d4d93df014e376048c41e30b3e46c2
maron <maronwhite6@gmail.com>
2019-03-05 17:56:14 +0300

    Single End Point for a Request

--- app/Http/Resources/ServiceRequest.php
+++ app/Http/Resources/ServiceRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Http\Resources\User as UserResource;
+use App\Http\Resources\Service as ServiceResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ServiceRequest extends JsonResource
@@ -14,6 +16,19 @@ class ServiceRequest extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        //return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'customer' => $this->customer,
+            'address' => $this->address,
+            'service' => $this->service,
+            'offer' => $this->offers,
+            'description' => $this->description,
+            'payment_method' => $this->payment_method,
+            'questions' => $this->questions,
+            'cancelled' => $this->cancelled,
+            'expected_start_date' => $this->expected_start_date,
+            'created_at' => $this->created_at,
+        ];
     }
 }
