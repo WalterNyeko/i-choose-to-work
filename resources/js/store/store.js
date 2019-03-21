a990d85b376f05db8dac553ce95115fc9c03e781
abbey <mugieabbeym@gmail.com>
2019-03-21 11:08:53 +0300

    adding devtools to store

--- resources/js/store/store.js
+++ resources/js/store/store.js
@@ -6,10 +6,18 @@ const initialState = {};
 
 const middleware = [thunk]
 
+let devtools = f => f
+if (process.browser && window.__REDUX_DEVTOOLS_EXTENSION__) {
+    devtools = window.__REDUX_DEVTOOLS_EXTENSION__()
+}
+
 const store = createStore(
     rootReducer, 
     initialState, 
-    applyMiddleware(...middleware),       
+    compose(
+        applyMiddleware(...middleware),
+        devtools
+    )   
 );
 
 export default  store;
