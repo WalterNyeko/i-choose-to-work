b4ee1e62a2183918d29d3a3f82641f276ac94ddf
maron <maronwhite6@gmail.com>
2019-03-28 15:20:54 +0300

    Add Vti to ichuzztowork

---
+++ database/migrations/2019_03_21_002552_create_vtis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVtisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vtis', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->unique();
+            $table->string('logo')->nullable();
+            $table->text('location')->nullable();
+            $table->longText('about')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vtis');
+    }
+}
